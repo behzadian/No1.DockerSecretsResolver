21b758048a4807e046e598d753fa99d6784a9d31
Hank <eqpc@hbn.dev>
2026-05-19 13:24:12 +0200

    Adding logger #3

--- lib/ConfigurationExtensions.cs
+++ lib/ConfigurationExtensions.cs
@@ -10,9 +10,21 @@ namespace No1.DockerSecretsResolver;
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2254:Template should be a static expression", Justification = "<Pending>")]
 public static class ConfigurationExtensions
 {
+	/// <summary>
+	/// Checkes the specified key for docker secret value.
+	/// If key was started with `/run/secrets/` and related file exists, loads secret from the file.
+	/// If key ends with `_FILE`, then puts the secret in a key without `_FILE` suffix. (ignores the case)
+	/// If key doesn't end with `_FILE` suffix, the changes value of itself with the secret
+	/// </summary>
+	/// <param name="configuration"></param>
+	/// <param name="key"></param>
+	/// <param name="logger">Can be null, but if not, logs the operations with Trace, Debug, Information levels</param>
+	/// <returns></returns>
 	public static bool ProcessDockerSecretKey(this IConfiguration configuration, string key, ILogger? logger = null) {
 		ArgumentNullException.ThrowIfNull(configuration);
 		ArgumentException.ThrowIfNullOrWhiteSpace(key);
+
+		logger?.LogTrace($"Processing key {key} for docker secrets.");
 
 		var value = configuration[key];
 
@@ -22,24 +34,53 @@ public static class ConfigurationExtensions
 		}
 
 		// If value looks like a Docker secret file path
-		if (value.StartsWith("/run/secrets/", StringComparison.OrdinalIgnoreCase) && File.Exists(value)) {
-			logger?.LogInformation($"Replacing value of {key}, with content of file {value}");
-			configuration[key] = File.ReadAllText(value).Trim();
+		if (value.StartsWith("/run/secrets/", StringComparison.OrdinalIgnoreCase)) {
+			if (!File.Exists(value)) {
+				logger?.LogDebug($"While {key} is similar to docker secret keys, but file `{value}` couldn't be found. Check volumes and key.");
+				return false;
+			}
+			var secret = File.ReadAllText(value).Trim();
 
 			if (key.EndsWith("_FILE", StringComparison.OrdinalIgnoreCase)) {
 				var prefixlessKey = key[..^"_FILE".Length];
 				logger?.LogInformation($"Replacing value of {prefixlessKey}, with content of file {value}");
+				configuration[prefixlessKey] = secret;
+			} else {
+				logger?.LogInformation($"Replacing value of {key}, with content of file {value}");
+				configuration[key] = secret;
 			}
 			return true;
 		} else {
+			logger?.LogTrace($"Key {key}'s value is not started with `/run/secrets/`.");
 			return false;
 		}
 	}
+
+	/// <summary>
+	/// Checks all keys in configurations, and if find any docker secret, loads and puts it in configurations with below rules:
+	/// If key was started with `/run/secrets/` and related file exists, loads secret from the file.
+	/// If key ends with `_FILE`, then puts the secret in a key without `_FILE` suffix. (ignores the case)
+	/// If key doesn't end with `_FILE` suffix, the changes value of itself with the secret
+	/// </summary>
+	/// <param name="configuration"></param>
+	/// <param name="onlyKeysWithSuffix">Only checks for keys that has `_FILE` suffix.</param>
+	/// <param name="loggerFactory">Can be null, but if not, creates a logger with [ConfigurationExtensions] key and logs the operations with Trace, Debug, Information levels</param>
+	/// <param name="skipKeys">Keys that being skipped and won't be checked for docker secrets.</param>
 	public static void ProcessDockerSecrets(this IConfiguration configuration, bool onlyKeysWithSuffix, ILoggerFactory? loggerFactory = null, params string[]? skipKeys) {
 		var logger = loggerFactory?.CreateLogger(typeof(ConfigurationExtensions));
 		ProcessDockerSecrets(configuration, onlyKeysWithSuffix, logger, skipKeys);
 	}
 
+	/// <summary>
+	/// Checks all keys in configurations, and if find any docker secret, loads and puts it in configurations with below rules:
+	/// If key was started with `/run/secrets/` and related file exists, loads secret from the file.
+	/// If key ends with `_FILE`, then puts the secret in a key without `_FILE` suffix. (ignores the case)
+	/// If key doesn't end with `_FILE` suffix, the changes value of itself with the secret
+	/// </summary>
+	/// <param name="configuration"></param>
+	/// <param name="onlyKeysWithSuffix">Only checks for keys that has `_FILE` suffix.</param>
+	/// <param name="logger">Can be null, but if not, logs the operations with Trace, Debug, Information levels</param>
+	/// <param name="skipKeys">Keys that being skipped and won't be checked for docker secrets.</param>
 	public static void ProcessDockerSecrets(this IConfiguration configuration, bool onlyKeysWithSuffix, ILogger? logger = null, params string[]? skipKeys) {
 		ArgumentNullException.ThrowIfNull(configuration);
 		logger?.LogTrace($"Processing configuration");
--- lib/README.md
+++ lib/README.md
@@ -1,2 +1,18 @@
 # No1.DockerSecretsUpdater
-Reads all settings and place that value of docker secrets in the original configurations
+
+Reads all settings and place that value of docker secrets in the configurations.
+
+This library provides 2 methods to check if a key is a docker secrets and put the secret in the configuration.
+
+- ProcessDockerSecrets.
+This method reads all keys in configurations, and if the key has not been skipped, processes the key
+
+
+- ProcessDockerSecretKey 
+This method processes specific key and if:
+-- Key's value started with `/run/secrets/`
+-- Specified file in value exists
+
+Then reads the file and put secret in:
+-- The key itself, if key does not end with `_FILE` (ignores case) suffix. (such as Connection::ConnectionString)
+-- Another key without `_FILE` suffix, if key ends with `_FILE` (ignores case) suffix. (so if the key was `Connection::ConnectionString_FILE`, the secret will be placed in `Connection::ConnectionString`)
